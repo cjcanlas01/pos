5e178615289a2386a2169d867579124d0e9f4e68
InternCJ <cjcanlas78@gmail.com>
2018-06-28 18:44:16 +0800

    First discussion version

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -21,7 +21,6 @@ class UsersController extends AppController
     public function index()
     {
         //$users = $this->paginate($this->Users);
-
         //$this->set(compact('users'));
         $this->render('/Pages/hpageblank');
     }
